52a61dbe22a0ede3fe4661c1662e101ee5698faf
abyssofbirds <deniskind@inbox.ru>
2022-01-13 14:17:42 +0300

    in test

--- index.js
+++ index.js
@@ -26,13 +26,13 @@
         ".one-slot-bt .button-in"
       );
 
-      const energy = parseFloat(
-        buttonSendToAdventure
-          .querySelector(".tool-body")
-          .textContent.split("/")[0]
-      );
+      // const energy = parseFloat(
+      //   buttonSendToAdventure
+      //     .querySelector(".tool-body")
+      //     .textContent.split("/")[0]
+      // );
 
-      if (button && energy > 5) {
+      // if (button && energy > 5) {
         button.click();
 
         // If button "close" in popup visible
@@ -44,79 +44,79 @@
 
         await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
 
-        // Click button "close" in popup
+      //   // Click button "close" in popup
         document.querySelector(".papper-modal .button-name-in").click();
 
         await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
-      }
+      // }
 
       // Check energy
-      if (energy <= 5) {
-        console.log("energy", energy);
-        let rarity = buttonSendToAdventure
-          .querySelector("ul li:nth-child(3)")
-          .textContent.split(": ")[1]
-          .toLowerCase();
-        if (rarity === "promo") rarity = "uncommon";
-        console.log("rarity", rarity);
-        document.querySelector(".menu-will-be .shop").click();
+      // if (energy <= 5) {
+      //   console.log("energy", energy);
+      //   let rarity = buttonSendToAdventure
+      //     .querySelector("ul li:nth-child(3)")
+      //     .textContent.split(": ")[1]
+      //     .toLowerCase();
+      //   if (rarity === "promo") rarity = "uncommon";
+      //   console.log("rarity", rarity);
+      //   document.querySelector(".menu-will-be .shop").click();
 
-        const foods = document.querySelectorAll(
-          ".shafl-wrap-in .one-shafl-prod .one-shafl-prod-thumb a"
-        );
-        const food = foods[foodsRating[rarity]];
+      //   const foods = document.querySelectorAll(
+      //     ".shafl-wrap-in .one-shafl-prod .one-shafl-prod-thumb a"
+      //   );
+      //   const food = foods[foodsRating[rarity]];
 
-        food.click();
+      //   food.click();
 
-        await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+      //   await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
 
-        document.querySelector(".modal-bay-bt .button-name-value").click();
+      //   document.querySelector(".modal-bay-bt .button-name-value").click();
 
-        // If button "close" in popup visible
-        while (
-          !document.querySelector("#thank-modal-shop .def-bt.mid").offsetParent
-        ) {
-          await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
-        }
+      //   // If button "close" in popup visible
+      //   while (
+      //     !document.querySelector("#thank-modal-shop .def-bt.mid").offsetParent
+      //   ) {
+      //     await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+      //   }
 
-        document.querySelector("#thank-modal-shop .def-bt.mid").click();
+      //   document.querySelector("#thank-modal-shop .def-bt.mid").click();
 
-        await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
+      //   await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
 
-        document
-          .querySelector(".header-menu-in .one-menu-item:nth-child(2) a")
-          .click();
+      //   document
+      //     .querySelector(".header-menu-in .one-menu-item:nth-child(2) a")
+      //     .click();
 
-        await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
+      //   await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
 
-        buttonSendToAdventure = document.querySelectorAll(
-          ".one-slot:not(.no-p):not(.dis)"
-        )[i];
+      //   buttonSendToAdventure = document.querySelectorAll(
+      //     ".one-slot:not(.no-p):not(.dis)"
+      //   )[i];
 
-        const buttonEnergy = buttonSendToAdventure.querySelector(
-          ".energy-bt.tooltip button"
-        );
-        console.log("buttonSendToAdventure", buttonSendToAdventure);
-        console.log("buttonEnergy", buttonEnergy);
+      //   const buttonEnergy = buttonSendToAdventure.querySelector(
+      //     ".energy-bt.tooltip button"
+      //   );
+      //   console.log("buttonSendToAdventure", buttonSendToAdventure);
+      //   console.log("buttonEnergy", buttonEnergy);
 
-        buttonEnergy.click();
+      //   buttonEnergy.click();
 
-        while (!document.querySelector(".modal-wrap-eat-list")?.offsetParent) {
-          await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
-        }
+      //   while (!document.querySelector(".modal-wrap-eat-list")?.offsetParent) {
+      //     await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+      //   }
 
-        await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
+      //   await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
 
-        const foodItem = [
-          ...document
-            .querySelector(".modal-wrap-eat-list")
-            .querySelectorAll(".bamb-modal-cards .one-eat-list.active"),
-        ].find((el) => el.textContent.toLowerCase().includes(rarity));
+      //   const foodItem = [
+      //     ...document
+      //       .querySelector(".modal-wrap-eat-list")
+      //       .querySelectorAll(".bamb-modal-cards .one-eat-list.active"),
+      //   ].find((el) => el.textContent.toLowerCase().includes(rarity));
 
-        foodItem.querySelector(".button-name-value").click();
+      //   foodItem.querySelector(".button-name-value").click();
 
-        await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
-      }
+      //   await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
+      // }
 
       await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
     }
