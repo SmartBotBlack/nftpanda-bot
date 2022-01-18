10f7a8bc1fca4394739834d45ca2a4e276ae6f39
abyssofbirds <deniskind@inbox.ru>
2022-01-18 17:42:05 +0300

    done. testing

--- index.js
+++ index.js
@@ -7,11 +7,9 @@
   while (1) {
     await new Promise((res) => setTimeout(res, PAUSE_TASK));
 
-    console.log(1);
-
     // Find all active button for mine
     const buttonsSendToAdventure = document.querySelectorAll(
-      ".one-slot:not(.no-p):not(.dis)"  // Количество панд 
+      ".one-slot:not(.no-p):not(.dis)"
     );
 
     // Going through all active buttons
@@ -20,37 +18,28 @@
         ".one-slot:not(.no-p):not(.dis)"
       )[i];
       const button = buttonSendToAdventure.querySelector(
-        ".one-slot-bt .button-in" //  кнопка отправки в путешествие
+        ".one-slot-bt .button-in"
       );
+
       if (button) {
         button.click();
-        console.log(2);
-      }
 
-      // If button "close" in popup visible
-      while (
-        !document.querySelector(".papper-modal .button-name-in")?.offsetParent 
-      ) {
-        await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
-      }
+        // If button "close" in popup visible
+        while (
+          !document.querySelector(".papper-modal .button-name-in")?.offsetParent
+        ) {
+          await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+        }
 
-      await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
+        await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
 
-      // Click button "close" in popup
+        // Click button "close" in popup
+        document.querySelector(".papper-modal .button-name-in").click();
 
-      while (document.querySelector(".papper-modal .button-name-in")) {
-        document.querySelector(".papper-modal .button-name-in").click(); // закрытие модального окна с с количесвтом намайненого. 
-        console.log(3);
+        await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
       }
 
       await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
-      console.log(4);
-
     }
   }
 })();
-
-
-// Скрипт работает один раз, второй раз в путшествие панды не отправляются
-// логи выводятся 1,2,3,4,1. То есть скрипт начинается заново сразу после задачи. 
-// Если скрипт отработал, через два часа ты жмешь send to adventure, то скрипт сам заканчивает задачу. 
