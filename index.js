7f6b7954ad5bc8296bc37fcefd3837aefbb1fdd0
Ilya Zyryanov <ilyazyry@mail.ru>
2022-02-25 23:07:48 +0700

    update bot

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
   const PAUSE_WAIT_POPUP = 10 * 1000;
   const PAUSE_BEFORE_CLOSE_POPUP = 10 * 1000;
 
+  const mouseClickEvents = ["mousedown", "click", "mouseup"];
+  function simulateMouseClick(element) {
+    mouseClickEvents.forEach((mouseEventType) =>
+      element.dispatchEvent(
+        new MouseEvent(mouseEventType, {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          buttons: 1,
+        })
+      )
+    );
+  }
+
   while (1) {
     await new Promise((res) => setTimeout(res, PAUSE_TASK));
 
@@ -24,21 +38,43 @@
       if (button) {
         button.click();
 
-        // If button "close" in popup visible
-        while (
-          !document.querySelector(".papper-modal .button-name-in")?.offsetParent
-        ) {
-          await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+        const location = document.querySelector("#group-adv-3.group-adv");
+        if (location) {
+          console.log("Wait location");
+          simulateMouseClick(location);
+
+          const sendAdventure = document.querySelector(
+            ".modal-to-adv.open.anim.bamb button"
+          );
+
+          if (sendAdventure) {
+            console.log("Wait sendAdventure");
+            sendAdventure.click();
+
+            // If button "close" in popup visible
+            while (
+              !document.querySelector(
+                ".modal-to-adv.reward-window.open.anim.bamboo-rew button"
+              )?.offsetParent
+            ) {
+              await new Promise((res) => setTimeout(res, PAUSE_WAIT_POPUP));
+            }
+
+            await new Promise((res) =>
+              setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP)
+            );
+
+            // Click button "close" in popup
+            document
+              .querySelector(
+                ".modal-to-adv.reward-window.open.anim.bamboo-rew button"
+              )
+              .click();
+
+            await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
+          }
         }
-
-        await new Promise((res) => setTimeout(res, PAUSE_BEFORE_CLOSE_POPUP));
-
-        // Click button "close" in popup
-        document.querySelector(".papper-modal .button-name-in").click();
-
-        await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
       }
-
       await new Promise((res) => setTimeout(res, PAUSE_BUTTON));
     }
   }
